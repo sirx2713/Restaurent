448d08960406d91a09d0d56fef3188f5116826b6
Sirx <bradx2713@gmail.com>
2024-03-03 08:36:16 +0200

    Initial Commit

---
+++ Restaurent/RestaurentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurentApp.swift
+//  Restaurent
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/03.
+//
+
+import SwiftUI
+
+@main
+struct RestaurentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
